4850ed12e338af223bc11e17a1c7ca8c6fa4be51
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-04-11 16:53:54 +0200

    WIP: inline field definitions

--- src/compileFieldDefinitions.py
+++ src/compileFieldDefinitions.py
@@ -19,7 +19,7 @@ def addLocalisation(jsonString, bundle="bso"):
 
 model = generator.loadSourceFromFile(inputFile)
 
-output = generator.generate(model, generator.JSON)
+output = generator.generate(model, generator.INLINE)
 
 #output = addLocalisation(output)
 
